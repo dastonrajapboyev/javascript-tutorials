92824f64e2a2e4263717b694573cb1c333c9edee
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-06-18 23:12:14 -0700

    switch case

---
+++ 3-lesson/index.js
@@ -0,0 +1,30 @@
+//=================== find ball
+
+// let ball = 95
+
+// switch (ball <100 && ball > 86) {
+//   case ball <100 && ball > 86:
+//     console.log("A");
+//     break;
+
+//   default:
+//     break;
+// }
+
+// ----------------------
+
+/*let meva = 'olma'
+
+switch (meva) {
+  case 'olma':
+    console.log(10000);
+    break;
+    case 'nok':
+      console.log(12000);
+      break;
+      case 'tarvuz':
+        console.log(20000);
+        break;
+    default :
+    console.log('bunday meva yoq')
+}*/
