bd76ffc4990d97a8bb1978d31cedddfc7166a9de
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-06-18 23:14:41 -0700

    switch case and if else

--- 3-lesson/index.js
+++ 3-lesson/index.js
@@ -28,3 +28,37 @@ switch (meva) {
     default :
     console.log('bunday meva yoq')
 }*/
+// if(-1 || 0) console.log( 'first' )
+// if(-1 && 0) console.log( 'second' )
+// if(null || -1 && 1) console.log( 'third' )
+/*
+let num = 15;
+
+if (num % 3 == 0 && num % 5 == 0){
+  console.log('3 ga xam 5 ga xam bolinadi')
+}else if(num % 3 == 0){
+  console.log('3 ga bolinadi')
+}else if (num % 5 == 0){
+  console.log('5 ga bolinadi')
+}else {
+  console.log('3 ga xam 5 ga xam bolinmaydigan son')
+}*/
+
+// Rewrite the 'switch' into an 'if'
+
+// let browser = 'Safari'
+
+// if (browser == 'Edge'){
+//   console.log("you've got the Edge");
+// }else if (browser == 'chrome' ||  browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+//   console.log('okay we support these browsers too');
+// }else {
+//   console.log( 'we hope that this page looks ok!' )
+// }
+
+//=======================
+// if (-1 || 0) console.log('first'); // first
+// if (-1 && 0) console.log('second');
+// if (null || -1 && 1)console.log('third'); // third
+
+// =======================
