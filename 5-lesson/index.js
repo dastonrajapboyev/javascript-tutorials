d8411ee7ac53e1f0d5894277b3695864472b50f3
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-06-23 23:40:38 -0700

    math methods without using method

---
+++ 5-lesson/index.js
@@ -0,0 +1,38 @@
+//  math methods
+// pow method
+// function g22pow (son, daraja){
+//   return son ** daraja
+// }
+// console.log(g22pow(5,2));
+
+//===============
+
+// sqrt method
+
+// function g22Sqrt(son)  {
+//   return son ** 0.5
+// }
+// console.log(g22Sqrt(25));
+
+// ==============
+
+// cbrt method
+
+// function cbrt (son){
+//   return son ** (1/3)
+// }
+// console.log(cbrt(64));
+// console.log(Math.ceil(cbrt(64)));
+
+// ======================
+
+// abs method 
+
+// function abs(son) {
+//   son = son * -1;
+//   return son
+// }
+// console.log(abs(-10));
+
+// =========================
+
