158103026c84c9c63d37f6957a3981e67167bb4b
dastonrajapboyev <dastonrajapboyev79@gmail.com>
2023-07-06 23:54:56 -0700

    finding object's keys, finding value which is a number, finding object's value that equal to boolean

---
+++ 8-lesson/task.js
@@ -0,0 +1,60 @@
+// object advanced
+
+// homeworks
+
+// No1 finding object's keys
+
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+// };
+// const func = (input) => {
+//   for (key in input) {
+//     console.log(key);
+//   }
+//   return input;
+// };
+// func(obj);
+
+// =========================================
+
+// no2
+// finding value which is a number
+
+// let obj = {
+//   id: 1,
+//   name: "WebBrain",
+//   age: 28 // I added age to check is function work dynamic or not
+// };
+
+// function func(par) {
+//     for(let key in par) {
+//       if (typeof par[key] == "number") {
+//         console.log(key);
+//       }
+//     }
+//     return par
+//   }
+//   func(obj);
+
+// =================================
+
+// finding object's value that equal to boolean
+// let obj = {
+//   id:1,
+//       name:"WebBrain",
+//       offline:true,
+//       online:true,
+//       individual:false
+// }
+// function func(par) {
+//   for(let key in par) {
+//     if (typeof par[key] == "boolean") {
+//         console.log(`${key}: ${par[key]}`);
+//     }
+//   }
+//   return par
+// }
+// func(obj);
+
+// ==================================
